cf06ec38d48c21f27b196f7ae4175d471f098b01
Mirthila <mirome@gmail.com>
2023-04-19 22:49:13 -0300

    PreEntrega2Romero

--- README.md
+++ README.md
@@ -23,3 +23,31 @@ Por último llamamos a la función validarCorreo
 -------------Nota para el profesor / Tutor-----------------
 
  El diseño del proyecto está en construcción, al no haber realizado el curso de desarrollo web no tengo proyecto base así que los HTML y CSS presentados van a ser modificado a lo largo del curso. 
+
+
+
+ -----------------*Segunda Pre-entrega*--------------------
+
+ En esta segunda pre-entrega cambié la manera de validar el correo usando arrays, objetos y funciones de orden superior. También mejoré los alert permitiendo saludar al usuario por su nombre.
+
+    Arrays:
+
+    "letrasPermitidas": es un arreglo que contiene todas las letras permitidas en la dirección de correo electrónico que se va a validar. Se utiliza para verificar si cada letra del correo ingresado es permitida o no.
+
+    "usuariosRegistrados": es un arreglo que contiene objetos de usuario registrados, cada uno de los cuales tiene un correo electrónico y un nombre asociado. Este arreglo se utiliza para buscar si un usuario ya está registrado con el correo electrónico ingresado. Si no está registrado se adicionará mediante el método push al array y se  saludará con el alert generado en la primera entrega mas la concatenación del nombre ingresado en el promp (¡Bienvenido a Berrylicious Jose!)
+
+
+    Objetos:
+
+    "usuarioRegistrado": es un objeto que contiene un correo electrónico y un nombre asociado. Este objeto se utiliza para buscar si un usuario ya está registrado con el correo electrónico ingresado.
+
+
+    Función de orden superior:
+
+    "Find": se utiliza para buscar un objeto específico dentro del arreglo "usuariosRegistrados" que tenga el correo electrónico ingresado y saber si ya está registrado.
+
+    -------------Nota para el profesor / Tutor 2-----------------
+
+En esta oportunidad fui mejorando algunas partes del css y adicioné ele un logo, nav y buscador al HTML. 
+
+dejé comentado el código anterior.
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* ------->Simulador de validación de mail<----------*/
+/* ------->Simulador de validación de mail<----------
 
 
 let validarCorreo = () => {
@@ -45,6 +45,59 @@ let validarCorreo = () => {
   }
 
   alert("¡Bienvenido a Berrylicious!");
+};
+
+validarCorreo(); */
+
+/*----------SEGUNDA PRE-ENTREGA--------*/
+
+
+let letrasPermitidas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+let correoValido = false;
+let usuariosRegistrados = [
+  { correo: "jose@gmail.com", nombre: "Jose" },
+  { correo: "ana@hotmail.com", nombre: "Ana" },
+  { correo: "juan@yahoo.com", nombre: "Juanjo" }
+];
+
+let validarCorreo = () => {
+  let correo = prompt("Por favor ingresa tu correo electrónico:");
+
+  let tieneArroba = false;
+  let tienePunto = false;
+  let tieneLetras = false;
+
+  for (let i = 0; i < correo.length; i++) {
+    if (correo[i] === "@") {
+      tieneArroba = true;
+    }
+    if (i >= 4 && letrasPermitidas.indexOf(correo[i]) !== -1) {
+      tieneLetras = true;
+    }
+    if (correo[i] === "." && tieneArroba && tieneLetras) {
+      tienePunto = true;
+      break;
+    }
+  }
+
+  correoValido = tieneArroba && tienePunto && tieneLetras;
+
+  if (correoValido) {
+    let usuarioRegistrado = usuariosRegistrados.find(usuario => usuario.correo === correo);
+    if (usuarioRegistrado) {
+      let nombre = prompt("Por favor ingresa tu nombre:");
+      usuarioRegistrado.nombre = nombre;
+      alert("Bienvenido de nuevo, " + usuarioRegistrado.nombre + "!");
+
+    } else {
+      let nombre = prompt("Por favor ingresa tu nombre:");
+      usuariosRegistrados.push({ correo: correo, nombre: nombre });
+      alert("¡Bienvenido a Berrylicious " + nombre + "!");
+    }
+  } else {
+    alert("Correo inválido, por favor inténtalo de nuevo.");
+  }
 };
 
 validarCorreo();
